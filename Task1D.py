281d5b41e7627d3256cccf14f60049676d7f6820
William Hawkins <wjh46@cam.ac.uk>
2022-01-22 20:23:20 +0000

    Commit start of 1D

--- Task1D.py
+++ Task1D.py
@@ -1,4 +1,28 @@
-# WH Created: 20/1/22 Modified: 22/1/22
-# IA Lent term computing module task 1D
-
+print("hello")
 from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_with_station
+
+def run():
+    '''requirements for task 1D'''
+
+    # Build list of stations
+    stations = build_station_list()
+    print (stations)
+
+    # list of rivers with a station
+    rivers = [] 
+    
+
+    for station in stations:
+        temp = bool(1)
+        for river in rivers:
+            if station.river == river:
+                temp = 0
+        if temp == 1:
+            rivers.append(station.river)
+
+    print (rivers)
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    run()
+run()
